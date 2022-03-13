d75ec938f7c8302efa7c99bc76e7b7c9294d871c
hchen2020 <holdenychen@gmail.com>
2022-03-13 16:21:55 -0700

    completed routes contained in controller.mjs

--- backend/controller.mjs
+++ backend/controller.mjs
@@ -1,4 +1,5 @@
 import express from 'express';
+import * as Exercise from './model.mjs';
 
 const app = express();
 const PORT = 3000;
@@ -8,36 +9,79 @@ app.use(express.json());
 // -----------------------------------
 
 // CRUD OPERATIONS:
-
-// -----------------------------------
 // CREATE
-app.post('/movies', (req, res) => {
-    movies.createMovie(req.body.title, req.body.year, req.body.language)
-        .then(movie => {
-            res.status(201).json(movie);
-        })
-        .catch(error => {
-            console.error(error);
-            // In case of an error, send back status code 400 in case of an error.
-            // A better approach will be to examine the error and send an
-            // error status code corresponding to the error.
-            res.status(400).json({ Error: 'Request failed' });
-        });
+app.post('/exercises', async (req, res) => {
+    const exercise = new Exercise({
+        name: req.body.name,
+        reps: req.body.reps,
+        weight: req.body.weight,
+        unit: req.body.unit,
+        date: req.body.date
+    });
+
+    try {
+        const newExercise = await exercise.save();
+        res.status(201).json(newExercise);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
 
 
 // -----------------------------------
 // READ
+app.get('/exercises', async (req, res) => {
+    try {
+        const exercises = await Exercise.find();
+        res.json(exercises);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 
 // -----------------------------------
 // UPDATE
+app.put('/exercises/:id', getExercise, async (req, res) => {
+    if (req.body.name !== null) {
+        res.exercise.name = req.body.name
+    }
+    try {
+        const updatedExercise = await res.exercise.save();
+        res.status(200).json(updatedExercise);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
 
 
 // -----------------------------------
 // DELETE
+app.delete('/exercises/:id', getExercise, async (req, res) => {
+    try {
+        await res.exercise.remove();
+        res.status(204).json({ message: 'Deleted exercise' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 
+// MIDDLEWARE FUNCTION
+async function getExercise(req, res, next) {
+    let exercise 
+    try {
+        exercise = await Exercise.findById(req);
+        if (subscriber === null ) {
+            return res.status(404).json({ message: 'Exercise not found' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.exercise = exercise
+    next();
+}
 
 
 // -----------------------------------
--- backend/model.mjs
+++ backend/model.mjs
@@ -1,25 +1,20 @@
 import mongoose from 'mongoose';
 
-// Prepare to the database movies_db in the MongoDB server running locally on port 
-27017
 mongoose.connect(
     "mongodb+srv://hchen:lull1ZE7ERoEPYDr@cs290.jdn0v.mongodb.net/exercises?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-
 // Connect to to the database
 const db = mongoose.connection;
 
-// The open event is called when the database connection successfully opens
 db.once("open", () => {
     console.log("Successfully connected to MongoDB using Mongoose!");
 });
 
+// ----------------------------------------------------------
 
-/**
- * Define the schema
- */
+// Define the schema
 const exerciseSchema = mongoose.Schema({
     name: { type: String, required: true },
     reps: { type: Number, required: true },
@@ -28,8 +23,18 @@ const exerciseSchema = mongoose.Schema({
     date: { type: String, required: true }  // MM-DD-YY format
 });
 
-
-/**
- * Compile the model from the schema. This must be done after defining the schema.
- */
+// Compile the model from the schema. This must be done after defining the schema.
 const exercise = mongoose.model("Exercise", exerciseSchema);
+
+// ----------------------------------------------------------
+// IMPLEMENT CRUD OPERATIONS IN THE MODEL
+
+
+// CREATE
+
+// READ
+
+// UPDATE
+
+// DELETE
+
